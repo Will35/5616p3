743302304742da42689cd02f3b21a3f2fcfee5ad
Yimin Wang <ywan0222@uni.sydney.edu.au>
2016-05-15 18:09:55 +1000

    Initial Commit

---
+++ 5616p3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  5616p3
+//
+//  Created by 王益民 on 5/15/16.
+//  Copyright © 2016 Wayne. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
